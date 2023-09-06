3512a23aa0134b406cb16eb145a4f540b3643fdc
Jaimie Ren <jaimier@cs.utexas.edu>
2023-09-06 15:11:26 -0500

    no more double freegit commit -m asd

--- eval.c
+++ eval.c
@@ -215,10 +215,10 @@ static void eval_node(node_t *nptr) {
                                 if (nptr->type == INT_TYPE) {
                                     nptr->val.ival = nptr->children[0]->val.ival + nptr->children[1]->val.ival;
                                 } else if (nptr->type == STRING_TYPE) {
-                                    nptr->val.sval = calloc( strlen(nptr->children[0]->val.sval)
-                                        + strlen(nptr->children[1]->val.sval) + 1 , sizeof(char));
-                                    nptr->val.sval = strcat(nptr->children[0]->val.sval, nptr->children[1]->val.sval);
-                                    nptr->val.sval[strlen(nptr->children[0]->val.sval) + strlen(nptr->children[1]->val.sval)] = '\0';
+                                    nptr->val.sval = malloc( strlen(nptr->children[0]->val.sval)
+                                        + strlen(nptr->children[1]->val.sval) + 1); 
+                                    nptr->val.sval[0] = '\0';
+                                    strcat(nptr->children[0]->val.sval, nptr->children[1]->val.sval);
                                 }
                                 break;
                             case TOK_BMINUS:
@@ -228,11 +228,11 @@ static void eval_node(node_t *nptr) {
                                 if(nptr->type == INT_TYPE) {
                                     nptr->val.ival = nptr->children[0]->val.ival * nptr->children[1]->val.ival;
                                 } else if (nptr->type == STRING_TYPE) {
-                                    nptr->val.sval = calloc(nptr->children[1]->val.ival * strlen(nptr->children[0]->val.sval) + 1, sizeof(char));
+                                    nptr->val.sval = malloc(nptr->children[1]->val.ival * strlen(nptr->children[0]->val.sval) + 1);
+                                    nptr->val.sval[0] = '\0';
                                     for(int i = 0; i < nptr->children[1]->val.ival; i++) {
                                         strcat(nptr->val.sval, nptr->children[0]->val.sval);
                                     }
-                                    nptr->val.sval[nptr->children[1]->val.ival * strlen(nptr->children[0]->val.sval)] = '\0';
                                 }
                                 break;
                             case TOK_DIV:
@@ -333,7 +333,7 @@ static void eval_node(node_t *nptr) {
                 default:
                     break;
             }
-        } else {
+        } else { // ternary
             eval_node(nptr->children[0]);
             if(nptr->children[0]->val.bval) {
                 eval_node(nptr->children[1]);
